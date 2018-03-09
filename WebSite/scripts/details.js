e6724e0e51a0718f19ed7a0d15bf3e4365354d73
Adi <adieduard@gmail.com>
2018-03-09 23:42:13 +0200

    web site first draft, landing page

---
+++ WebSite/scripts/details.js
@@ -0,0 +1,22 @@
+﻿var getParameterByName = function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+var app = angular.module("depro", []);
+
+app.controller("rentable", function ($scope) {
+    var id = getParameterByName("id");
+    var selected;
+    angular.forEach(rentables, function (item) {
+        if (item.Id == id) {
+            selected = item;
+        }   
+    });
+    $scope.rentable = selected;
+});
---
+++ WebSite/scripts/main.js
@@ -0,0 +1,70 @@
+﻿
+var app = angular.module("depro", []);
+
+app.filter('matchRentables', function () {
+    return function (items, freetext, roomsfilter, floorfilter, typefilter) {
+        var filtered = [];
+        angular.forEach(items, function (item) {
+            if (item.Name.toLowerCase().indexOf(freetext.toLowerCase()) != -1) {
+                filtered.push(item);
+            } else if (freetext != '' && freetext != null && item.Description.toLowerCase().indexOf(freetext.toLowerCase()) != -1) {
+                filtered.push(item);
+            } else if (freetext == '' || freetext == null) {
+                filtered.push(item);
+            }
+        });
+
+        var floorfiltered = [];
+        angular.forEach(filtered, function (item) {
+            if (item.FloorNumber == floorfilter) {
+                floorfiltered.push(item);
+            } else if (floorfilter == '' || floorfilter == null) {
+                floorfiltered.push(item);
+            }
+        });
+
+        var roomfiltered = [];
+        angular.forEach(floorfiltered, function (item) {
+            if (item.Rooms == roomsfilter) {
+                roomfiltered.push(item);
+            } else if (roomsfilter == '' || roomsfilter == null) {
+                roomfiltered.push(item);
+            }
+        });
+
+        var typefiltered = [];
+        angular.forEach(roomfiltered, function (item) {
+            if (item.Type == typefilter) {
+                typefiltered.push(item);
+            } else if (typefilter == '-1' || typefilter == null) {
+                typefiltered.push(item);
+            }
+        });
+
+        return typefiltered;
+    };
+});
+
+app.controller("rentables", function ($scope) {
+    $scope.rentables = rentables;
+    $scope.freetext = '';
+    $scope.roomsfilter = '';
+    $scope.floorfilter = '';
+    $scope.typefilter = '-1';
+    $scope.haselevator = false;
+    $scope.hasparking = false;
+    $scope.provideinvoice = false;
+    $scope.kitcheniland = false;
+    $scope.luguriesbath = false;
+    $scope.lugurieslivingroom = false;
+    $scope.Luguriesbedroom = false;
+
+});
+
+$(function () {
+    // JS only for the switch
+    $("#switch-view").click(function () {
+        $(this).find("button").toggleClass("active");
+        $(".article-wrapper").toggleClass("bloc col-xs-12 col-xs-4");
+    });
+});
