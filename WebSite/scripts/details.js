d616262cf216a222a997c9e43865364b3a6860a1
Adi <adieduard@gmail.com>
2018-03-10 03:31:43 +0200

    slideshow in details page

--- WebSite/scripts/details.js
+++ WebSite/scripts/details.js
@@ -8,7 +8,7 @@
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
 
-var app = angular.module("depro", []);
+var app = angular.module("depro", ["ngAnimate"]);
 
 app.controller("rentable", function ($scope) {
     var id = getParameterByName("id");
@@ -16,7 +16,20 @@ app.controller("rentable", function ($scope) {
     angular.forEach(rentables, function (item) {
         if (item.Id == id) {
             selected = item;
-        }   
+        }
     });
     $scope.rentable = selected;
+    $scope.currentIndex = 0;
+    $scope.next = () => {
+        $scope.currentIndex < $scope.rentable.Images.length - 1 ? $scope.currentIndex++ :
+            $scope.currentIndex = 0;
+    };
+
+    $scope.prev = () => {
+        $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex =
+            $scope.rentable.Images.length - 1;
+    };
+    $scope.showImage = (index) => {
+        return index === $scope.currentIndex;
+    };
 });
